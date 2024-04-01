388b1bb48dbe505fdf940910ace4cd0289b0531c
darkavenger <s23315@pjwstk.edu.pl>
2024-04-01 12:50:50 +0200

    Storybook controls work fine

--- my-app/src/stories/TagTable.stories.ts
+++ my-app/src/stories/TagTable.stories.ts
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { TagTable } from './TagTable';
+import { number } from 'prop-types';
 
 const meta = {
   title: 'Example/TagTable',
@@ -10,7 +11,34 @@ const meta = {
     // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
     layout: 'fullscreen',
   },
+  args: {
+
+  },
 } satisfies Meta<typeof TagTable>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
+
+export const Small: Story = {
+  args: {
+    page: 0,
+    pageSize: 5,
+    API_URL: 'https://api.stackexchange.com/2.3/tags?site=stackoverflow',
+  },
+};
+
+export const Big: Story = {
+  args: {
+    page: 0,
+    pageSize: 10,
+    API_URL: 'https://api.stackexchange.com/2.3/tags?site=stackoverflow',
+  },
+};
+
+export const Error: Story = {
+  args: {
+    page: 0,
+    pageSize: 5,
+    API_URL: 'https://api.stackexchange.com/wrongurl',
+  },
+};
--- my-app/src/stories/TagTable.tsx
+++ my-app/src/stories/TagTable.tsx
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import { Skeleton } from '@mui/material';
+import { Alert, Skeleton } from '@mui/material';
 import { Tag } from '../types/tag';
+import ErrorIcon from '@mui/icons-material/Error';
 
 interface TagTableProps {
-  page?: number,
   /**
    * Page display
    */
-  pageSize?: number,
+  page?: number,
   /**
    * Amount of records on a single page
    */
+  pageSize?: number,
+  /**
+   * API URL to fetch data from
+   */
+  API_URL?: string,
 }
 
 /**
@@ -21,16 +26,34 @@ interface TagTableProps {
 export const TagTable = ({
   page = 0,
   pageSize = 5,
+  API_URL = 'https://api.stackexchange.com/2.3/tags?site=stackoverflow',
 }: TagTableProps) => {
   const [ data, setData ] = useState<Tag[]>();
+  const [ loading, setLoading ] = useState(true);
+  const [ error, setError ] = useState(null);
   useEffect(() => {
-    axios.get('https://api.stackexchange.com/2.3/tags?site=stackoverflow')
-      .then(result => result.data.items.map((item: Tag, index: number) => ({
-        ...item,
-        id: index + 1,
-      })))
-      .then(result => setData(result))
-      .catch((error) => console.log(error));
+    axios.get(API_URL)
+      .then(result => {
+        if (result && result.data && result.data.items) {
+          return result.data.items.map((item: any, index: number) => ({
+            ...item,
+            id: index + 1,
+          }));
+        } else {
+          throw new Error('Data not available');
+        }
+      })
+      .then(result => {
+        if (result) {
+          setData(result);
+        } else {
+          throw new Error('Result is undefined');
+        }
+      })
+      .catch((error) => {
+        setError(error.response ? error.response.status : 'Network Error');
+      })
+      .finally(() => setLoading(false));
   }, []);
 
   const columns: GridColDef[] = [
@@ -40,19 +63,22 @@ export const TagTable = ({
   ];
 
   return (
-    <div className="mx-auto w-1/2 pt-10 text-center">
-      {data ? <DataGrid
-        rows={data}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: page, pageSize: pageSize },
-          },
-        }}
-        pageSizeOptions={[ 5, 10 ]}
-        checkboxSelection
-      /> : <Skeleton variant="rectangular" width={700} height={400} />
-      }
+    <div>
+      <div className="mx-auto w-1/2 pt-10 text-center">
+        {!loading && data ? <DataGrid
+          rows={data}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: page, pageSize: pageSize },
+            },
+          }}
+          pageSizeOptions={[ 5, 10 ]}
+          checkboxSelection
+        /> : <Skeleton variant="rectangular" width={700} height={400}/>
+        }
+        {error !== null && <Alert severity="error"> Error: {error}</Alert> }
+      </div>
     </div>
   );
 
